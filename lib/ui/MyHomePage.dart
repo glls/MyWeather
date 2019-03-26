5e5b01385fc56277697c49b2bc5f55d0bea9f1a8
George Litos <glitos@schoox.com>
2019-03-26 16:42:10 +0200

    mockup UI complete

--- lib/ui/MyHomePage.dart
+++ lib/ui/MyHomePage.dart
@@ -19,9 +19,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -31,13 +29,14 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.lightBlue,
         appBar: AppBar(
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
-        body
-        : Weather()// This trailing comma makes auto-formatting nicer for build methods.
+        body:
+            Weather() // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/ui/Weather.dart
+++ lib/ui/Weather.dart
@@ -4,15 +4,89 @@ import 'package:intl/intl.dart';
 class Weather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget dateSection = Container(
+      child: Text(
+        DateFormat("MMMM d, HH:mm").format(DateTime.now()),
+//        DateFormat.yMMMMEEEEd().format(DateTime.now()),
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+      ),
+    );
+
+    Widget tempSection = Container(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            '20',
+            style: TextStyle(
+              fontSize: 80.0,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(
+                top: 12.0,
+              ),
+              margin: EdgeInsets.only(
+                left: 6.0,
+              ),
+              child: Text(
+                '\u2103',
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+            ),
+          ),
+          Image.asset(
+            'assets/img/cloudy.png',
+            width: 100.0,
+            height: 100.0,
+            fit: BoxFit.cover,
+          )
+        ],
+      ),
+    );
+
+    Widget descriptionSection = Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Tokyo',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        Text(
+          'Cloudy',
+          style: TextStyle(
+            fontSize: 24.0,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ));
+
     return Container(
       padding: const EdgeInsets.all(60.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-
+          dateSection,
+          tempSection,
+          descriptionSection,
         ],
       ),
     );
   }
-
 }
